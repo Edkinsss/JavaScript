cf599e2d16a069b90fda12f20d7080b4c5a249c6
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-14 16:07:58 +0300

    Применение преобразования к строкам в JavaScript

---
+++ Задание 33/33.js
@@ -0,0 +1,8 @@
+let num1 = 1234;
+let num2 = 456789;
+let str1 = String(num1);
+let str2 = String(num2); 
+
+alert(str1.length);
+alert(str2.length);
+alert(String(num1).length + String(num2).length);
