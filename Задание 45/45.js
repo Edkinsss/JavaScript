919378b79ce63c8a336016b437110087bb301c97
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-15 22:13:08 +0300

    Длина массива в JavaScript

--- Задание 45/45.js
+++ Задание 45/45.js
@@ -1,3 +1,11 @@
 let arr = String('a+b+c+d');
 alert(arr);
 console.log(arr);
+
+let art = [1, 2, 3];
+alert(art[0] + art[1] + art[2]);
+
+let arw = [1, 2, 3];
+alert(arw[0]);
+alert(arw[1]);
+alert(arw[2]);
---
+++ Задание 46/46.js
@@ -0,0 +1,6 @@
+let arr = ['a', 'b', 'c', 'n', 'h', 'u', 1, 45, false];
+console.log(arr[arr.length - 1]);
+
+let art = ['a', 'b', 'c', 'n', 'h', 'u', 1, 45, false];
+console.log(art.length);
+alert(art.length)
