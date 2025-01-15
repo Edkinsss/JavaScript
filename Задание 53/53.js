97dfae6be36a0a0b65e842480bed9f5e01f379f9
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-16 01:41:53 +0300

    Ключи массивов из переменных в JavaScript

---
+++ Задание 53/53.js
@@ -0,0 +1,9 @@
+let arr = ['1', '2', '3', '4', '5'];
+let key = 2;
+console.log(arr[key]);
+
+let kkk = ['A', 'B', 'C', 'V', 'B'];
+let key1 = 2;
+let key2 = 4;
+
+console.log(kkk[key1] + kkk[key2]);
