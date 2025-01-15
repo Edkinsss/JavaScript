194c8b39c38a8be5aae893c155a6fbecc344def0
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-15 20:49:54 +0300

    Вывод текста в документ в JavaScript

---
+++ Задание 42/42.js
@@ -0,0 +1,7 @@
+let str = 'text editor';
+document.write('<i>' + str + '</i>');
+document.write('<li>1</li>');
+document.write('<li>2</li>');
+document.write('<li>3</li>');
+document.write('<li>4</li>');
+document.write('<li>5</li>');
