522397297ad8f7bf6348ebe19008a8303921f7f0
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-16 17:02:15 +0300

    Логическое И в JavaScript

---
+++ Задание 76/76.js
@@ -0,0 +1,15 @@
+let num1 = 0;
+let num2 = 3;
+
+if (num1 <= 1 && num2 >= 3) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+let num = 10;
+if (num >=10 && num <=20) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
