1a6c58499922870aaf907f4b0991adabc5ab3c4e
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-18 23:04:49 +0300

    Конструкция else if в JavaScript

---
+++ Задание 85/85.js
@@ -0,0 +1,11 @@
+let day = 30;
+
+if (day == 1 - 10) {
+    console.log('1 декада(1 - 10 дней)');
+} else if (day ==10 - 20) {
+    console.log('2 декада (10 - 20 дней)');
+} else if (day == 30) {
+    console.log('3 декада (20 - 30 дней)');
+} else {
+    console.log('Ни в одну декаду, дата не попала');
+}
