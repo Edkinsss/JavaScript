a9a2e6feabb4d66600d27a8a8acf743bbea60ecb
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-18 23:59:37 +0300

    Вложенные конструкции if-else в JavaScript

---
+++ Задание 86/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+let str = '78'; 
+let num = Number(str);
+
+if (num >= 10 && num <= 99) {
+    let sum = Number(str[0]) + Number(str[1]);
+    console.log((sum +  'сумма цифр'));
+
+    if (sum <= 9) {
+        console.log('Сумма цифр однозначна');
+    } else {
+        console.log('Сумма цифр двухзначна');
+    }
+} else {
+    console.log('Число не попадает в диапазон от 10 до 99.');
+}
