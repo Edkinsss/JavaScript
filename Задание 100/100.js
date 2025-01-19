e03480e29d13494907c91b7bb761d80fcddc404a
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 12:14:19 +0300

    Практика на условия if-else в JavaScript

---
+++ Задание 100/100.js
@@ -0,0 +1,49 @@
+let num = '123033';
+let sum1 = num[0] + num[1] + num[2];
+let sum2 = num[3] + num[4] + num[5];
+
+if (sum1 = sum2) {
+	console.log('Да');
+} else {
+	console.log('Нет');
+}
+
+
+
+let str = '123';
+let num1 = Number(str);
+
+console.log(Number(str[0]) + Number(str[1]) + Number(str[2]));
+
+
+
+let txt = '12345';
+
+if (txt[0] == '1' || txt[0] == '2' || txt[0] == '3') {
+    console.log('Попали!')
+} else {
+    console.log('Не Попали!')
+}
+
+
+let txt1 = 'abcde';
+
+if (txt1[0] == 'a') {
+    console.log('Попали!')
+} else {
+    console.log('Не Попали!')
+}
+
+let month = 4;
+
+if (month >= 0 && month <= 1) {
+    console.log('1 четверть');
+} else if (month >= 1 && month <= 2) {
+    console.log('2 четверть');
+} else if (month >= 2 && month <= 3) {
+    console.log('3 четверть');
+} else {
+    console.log('4 четверть');
+}
+
+
---
+++ Задание 100/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let month = 4;
+
+if (month >= 0 && month <= 1) {
+    console.log('1 четверть');
+} else if (month >= 1 && month <= 2) {
+    console.log('2 четверть');
+} else if (month >= 2 && month <= 3) {
+    console.log('3 четверть');
+} else {
+    console.log('4 четверть');
+}
