c723f2858b259f07d643227525fa3a21654aea22
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 13:04:39 +0300

    Цикл while в JavaScript

---
+++ Задание 103/103.js
@@ -0,0 +1,24 @@
+let num = 10;
+let count = 0;
+
+while (num <= 1000) {
+    num *= 3
+    count++
+}
+console.log(num);
+console.log(count);
+
+
+let number = 10;
+
+while (number >= 9 && number < 33) {
+    number++;
+    console.log(number);
+}
+
+let number1 = 0;
+
+while (number1 >= 0 && number1 < 100) {
+    number1++;
+    console.log(number1);
+}
---
+++ Задание 103/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let number1 = 0;
+
+while (number1 >= 0 && number1 < 100) {
+    number1++;
+    console.log(number1);
+}
