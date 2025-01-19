46d4d7fc29f3fef71936895f3ea2ced68ec23b30
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 13:26:46 +0300

    Цикл for JavaScript

---
+++ Задание 104/104.js
@@ -0,0 +1,21 @@
+for (let i = 100; i > 0; i--) {
+    console.log(i);
+}
+
+
+for (let a = 1; a <= 99; a += 2) {
+    console.log(a);
+}
+
+for (let a = 2; a <= 100; a += 2) {
+    console.log(a);
+}
+
+for (let b = 11; b <= 33; b++) {
+    console.log(b)
+}
+
+for (let c = 1; c <= 100; c++) {
+    console.log(c);
+}
+
