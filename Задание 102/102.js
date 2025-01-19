10ecfb81fbcd238c376d908ac3501566d70d32ae
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 12:27:57 +0300

    Перебор объекта циклом for-in в JavaScript

---
+++ Задание 102/102.js
@@ -0,0 +1,11 @@
+let obj = {x: 1, y: 2, z: 3};
+
+for (let ofr in obj) {
+    console.log(ofr) + console.log(obj[ofr])
+}
+
+let obj1 = {x: 1, y: 2, z: 3};
+
+for (let ooo in obj1) {
+    console.log(ooo);
+}
---
+++ Задание 102/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let obj1 = {x: 1, y: 2, z: 3};
+
+for (let ooo in obj1) {
+    console.log(ooo);
+}
