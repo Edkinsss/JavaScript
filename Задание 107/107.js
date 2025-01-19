fb2cb74b5fc9984ec2653d00c9442c11281a9488
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 14:08:31 +0300

    Накопление чисел в циклах JavaScript

---
+++ Задание 107/107.js
@@ -0,0 +1,18 @@
+let res = 1;
+
+for (let i = 1; i <= 20; i++) {
+    res = res * i;
+}
+console.log(res);
+
+let yes = 0;
+for (let a = 1; a <= 99; a+=2) {
+    yes +=a;
+}
+console.log(yes);
+
+let no = 0;
+for (let q = 2; q <= 100; q+=2) {
+    no += q;
+}
+console.log(no);
---
+++ Задание 107/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let no = 0;
+for (let q = 2; q <= 100; q+=2) {
+    no += q;
+}
+console.log(no);
