8f685d4ee6886609188355c48890e6ef6d1405bd
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 10:36:43 +0300

    Функция confirm в JavaScript

---
+++ Задание 91/91.js
@@ -0,0 +1,9 @@
+
+let userChoice = confirm("Вам есть 18?");
+
+
+if (userChoice) {
+    console.log("Пользователь нажал 'ОК'.");
+} else {
+    console.log("Пользователь нажал 'Отмена. Доступ запрещен'.");
+}
