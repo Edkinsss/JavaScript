31071a4459edfe885d6c466bc2d229c112158536
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 11:28:44 +0300

    Проверка символов строки в JavaScript

---
+++ Задание 96/96.js
@@ -0,0 +1,16 @@
+let str = 'abcde';
+
+if (str[0] == 'a' || str[0] == 'b') {
+    console.log('Правильно !')
+} else {
+    console.log('Неверно !')
+}
+
+let str1 = 'yyyyx';
+let last = str1[str1.length - 1];
+
+if (last == 'x') {
+	console.log('!');
+} else {
+    console.log('---')
+}
---
+++ Задание 96/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let str1 = 'yyyyx';
+let last = str1[str1.length - 1];
+
+if (last == 'x') {
+	console.log('!');
+} else {
+    console.log('---')
+}
