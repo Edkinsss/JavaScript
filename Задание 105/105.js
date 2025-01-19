03f70aecb0c64d60e909c611baa50f739d015ed9
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 13:45:21 +0300

    Цикл for для массивов в JavaScript

---
+++ Задание 105/105.js
@@ -0,0 +1,22 @@
+let arr = ['a', 'b', 'c', 'd', 'e'];	
+for (let i = 0; i <= arr.length - 1; i++) {
+	console.log(arr[i]);
+}
+
+
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+for (let c = arr1.length - 1; c >= 0; c--) {
+    console.log(arr1[c])
+}
+
+
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+for (let d = 1; d < arr2.length - 1; d++) {
+    console.log(arr2[d]);
+}
+
+
+let arr3 = ['a', 'b', 'c', 'd', 'e'];
+for (let u = 0; u < arr3.length; u++) {
+    console.log(arr3[u]);
+}
---
+++ Задание 105/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let arr3 = ['a', 'b', 'c', 'd', 'e'];
+for (let u = 0; u < arr3.length; u++) {
+    console.log(arr3[u]);
+}
