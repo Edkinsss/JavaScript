f88e8b00d1d9200b68f4c70d1116d0947f0da6df
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 11:21:05 +0300

    Проверка длины строк и массивов в JavaScript

---
+++ Задание 95/95.js
@@ -0,0 +1,8 @@
+let str = '12345';
+let num = Number(str); 
+
+if (str.length >= 3) {
+	alert((Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3]) + Number(str[4])));
+} else {
+    alert('Не верно !')
+}
