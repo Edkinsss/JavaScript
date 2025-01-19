7073157928f4f3d436a55c0bfbd7806a63b9940e
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-19 10:50:15 +0300

    Нюансы области видимости переменных в конструкциях if-else в JavaScript

---
+++ Задание 93/93.js
@@ -0,0 +1,32 @@
+let age = 19;
+let res;
+
+if (age >= 18) {
+	res;
+	
+	if (age <= 23) {
+		res = 'от 18 до 23';
+	} else {
+		res = 'больше 23';
+	}
+} else {
+	res = 'меньше 18';
+}
+
+console.log(res);
+
+
+let age1 = 24;
+let res1;
+
+if (age1 >= 18) {
+	if (age1 <= 23) {
+		res1 = 'от 18 до 23';
+	} else {
+		res1 = 'больше 23';
+	}
+} else {
+	res1 = 'меньше 18';
+}
+
+console.log(res1);
---
+++ Задание 93/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+let age1 = 24;
+let res1;
+
+if (age1 >= 18) {
+	if (age1 <= 23) {
+		res1 = 'от 18 до 23';
+	} else {
+		res1 = 'больше 23';
+	}
+} else {
+	res1 = 'меньше 18';
+}
+
+console.log(res);
