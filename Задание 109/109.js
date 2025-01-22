f7fa3bb496e20c4d7c3d47eaf2dacf1351663b4c
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-22 16:08:48 +0300

    Формирование строк через циклы в JavaScript

---
+++ Задание 109/109.js
@@ -0,0 +1,28 @@
+let m = '';
+
+for (let i = 0; i < 5; i++) {
+    m += '-';
+}
+console.log(m);
+
+let b = '';
+
+for (let q = 1; q < 10; q++) {
+    b += q;
+}
+console.log(b);
+
+let e = '';
+
+for (let a = 9; a > 0; a--) {
+    e += a;
+}
+console.log(e);
+
+
+let w = '';
+
+for (let x = 1; x < 10; x++) {
+    w = w + '-' + x;
+}
+console.log(w);
---
+++ Задание 109/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let w = '';
+
+for (let x = 1; x < 10; x++) {
+    w = w + '-' + x;
+}
+console.log(w);
