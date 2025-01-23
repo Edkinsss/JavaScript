243cd572ab94ea674071aa09eb2f7eccedb1d37d
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-23 13:51:37 +0300

    Вложенные циклы в JavaScript

--- Задание 112/112.js
+++ Задание 112/112.js
@@ -7,7 +7,6 @@ for (let i = 1; i <= 9; i++){
 
 for (let i = 11; i <= 33; i++) {
     for (let j = 1; j < 1; j++) {
-        document.write(i + ' ')
     }
     if (i == 14 ||  i == 24 || i == 34) {
         i += 7;
