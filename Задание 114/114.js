8b6e39ed5af7b0fdabbf1a8f37b8a98a63995716
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-23 14:21:06 +0300

    Изменение массива в цикле JavaScript

---
+++ Задание 114/114.js
@@ -0,0 +1,24 @@
+let arr = [1,5,8,3,7]
+
+for (let i = 0; i < arr.length; i++) {
+    arr[i] += 10;
+}
+console.log(arr);
+
+
+
+let arr1 = [1,5,8,3,7]
+
+for (let i = 0; i < arr1.length; i++) {
+    arr1[i] -= 1;
+}
+console.log(arr1);
+
+
+
+let arr2 = [1,5,8,3,7]
+
+for (let i = 0; i < arr2.length; i++) {
+    arr2[i] **= 2;
+}
+console.log(arr2);
---
+++ Задание 114/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let arr2 = [1,5,8,3,7]
+
+for (let i = 0; i < arr2.length; i++) {
+    arr2[i] **= 2;
+}
+console.log(arr2);
