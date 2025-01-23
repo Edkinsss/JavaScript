d3c6f40ae8b8fd49974de1c8a2760314449a6d52
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-01-23 13:49:15 +0300

    Вложенные циклы в JavaScript

---
+++ Задание 112/112.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 9; i++){
+    for (let j = 1; j <= 3; j++){
+        console.log(i);
+    }
+}
+
+
+for (let i = 11; i <= 33; i++) {
+    for (let j = 1; j < 1; j++) {
+        document.write(i + ' ')
+    }
+    if (i == 14 ||  i == 24 || i == 34) {
+        i += 7;
+    }
+    document.write(i + ' ')
+}
---
+++ Задание 112/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+for (let i = 11; i <= 33; i++) {
+    for (let j = 1; j < 1; j++) {
+        if (i == 13 || i == 23 || i == 33) {
+            i += 8;
+            document.write(i + ' ')
+        }
+    }
+    document.write(i + ' ')
+}
